3af2be32708833c28daa3de583fe1dbdffe08322
Yevhen-Pashkulskyi <pashkulskyevhen@gmail.com>
2024-05-15 23:27:34 +0300

    add entity product

---
+++ src/main/java/org/example/entity/Product.java
@@ -0,0 +1,90 @@
+package org.example.entity;
+
+import org.springframework.stereotype.Component;
+
+import java.util.Objects;
+
+@Component
+
+public class Product {
+    private Integer id;
+    private String name;
+    private Integer quota;
+    private Double price;
+
+    public Product(String name, int quota, double price) {
+        this.name = name;
+        this.quota = quota;
+        this.price = price;
+    }
+
+    public Product(int id, String name, int quota, double price) {
+        this.id = id;
+        this.name = name;
+        this.quota = quota;
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+
+        if (!Objects.equals(id, product.id)) return false;
+        if (!Objects.equals(name, product.name)) return false;
+        if (!Objects.equals(quota, product.quota)) return false;
+        return Objects.equals(price, product.price);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (quota != null ? quota.hashCode() : 0);
+        result = 31 * result + (price != null ? price.hashCode() : 0);
+        return result;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQuota() {
+        return quota;
+    }
+
+    public void setQuota(int quota) {
+        this.quota = quota;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", quota=" + quota +
+                ", price=" + price +
+                '}';
+    }
+}
