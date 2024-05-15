5c50f955725a407254a7d6cc0f2340c70fbc4ca1
Yevhen-Pashkulskyi <pashkulskyevhen@gmail.com>
2024-05-15 23:39:48 +0300

    add entity product mapper

--- src/main/java/org/example/entity/Product.java
+++ src/main/java/org/example/entity/Product.java
@@ -12,6 +12,9 @@ public class Product {
     private Integer quota;
     private Double price;
 
+    public Product(){
+    }
+
     public Product(String name, int quota, double price) {
         this.name = name;
         this.quota = quota;
