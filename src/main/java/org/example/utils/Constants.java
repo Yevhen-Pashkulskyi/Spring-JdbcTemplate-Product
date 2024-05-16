0cd61e0ad5cd958f46eec169f3b442a57f6e443b
Yevhen-Pashkulskyi <pashkulskyevhen@gmail.com>
2024-05-16 21:41:44 +0300

    add constants

---
+++ src/main/java/org/example/utils/Constants.java
@@ -0,0 +1,16 @@
+package org.example.utils;
+
+public final class Constants {
+    private Constants() {}
+    public static final String ID_ERR_MSG = "id error";
+    public static final String NAME_ERR_MSG = "name error";
+
+    public final static String DATA_ABSENT_MSG = "\n>> No data!";
+    public final static String DATA_INSERT_MSG = "\n>> Created.";
+    public final static String DATA_UPDATE_MSG = "\n>> Updated.";
+    public final static String DATA_DELETE_MSG = "\n>> Deleted.";
+    public final static String APP_CLOSE_MSG = "\n>> App closed.";
+    public final static String INPUT_REQ_MSG = "Input required.";
+    public final static String INCORRECT_VALUE_MSG = ">> Incorrect value! Try again...";
+    public final static String SMTH_WRONG_MSG = "\n>> Something wrong.";
+}
