ff6002bf08f80617723ad369d614194d1eb6eaef
Yevhen-Pashkulskyi <pashkulskyevhen@gmail.com>
2024-05-16 22:55:56 +0300

    add app view

---
+++ src/main/java/org/example/view/AppView.java
@@ -0,0 +1,40 @@
+package org.example.view;
+
+import org.example.utils.Constants;
+import org.springframework.stereotype.Component;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+@Component
+public class AppView {
+    Scanner sc = new Scanner(System.in);
+
+    public String getOption(){
+        showMenu();
+        String option = "";
+        try{
+            option = sc.nextLine().trim();
+        }catch (InputMismatchException e){
+            System.out.println(Constants.INCORRECT_VALUE_MSG);
+        }
+        return option;
+    }
+    public void showMenu(){
+        System.out.print("""
+                Menu
+                1- Create Product
+                2- Show All Products
+                3- Show product by ID
+                4- Update product
+                5- Delete product
+                0- Exit program.""");
+        System.out.println("Input your choice: ");
+    }
+    public void getOutput(String output){
+        if(output.equals(Constants.APP_CLOSE_MSG))
+            System.out.println(output);
+        sc.close();
+        System.exit(0);
+    }
+}
