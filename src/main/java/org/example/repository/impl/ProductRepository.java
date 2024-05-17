4ae41629558b39ee43d15724ae8336bca4f77f13
Yevhen-Pashkulskyi <pashkulskyevhen@gmail.com>
2024-05-17 10:22:28 +0300

    add product repository

---
+++ src/main/java/org/example/repository/impl/ProductRepository.java
@@ -0,0 +1,76 @@
+package org.example.repository.impl;
+
+import org.example.entity.Product;
+import org.example.entity.ProductMapper;
+import org.example.repository.AppRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class ProductRepository implements AppRepository<Product> {
+    private final Product product;
+    JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public ProductRepository(DataSource dataSource, Product product) {
+        jdbcTemplate = new JdbcTemplate(dataSource);
+        this.product = product;
+    }
+
+    @Override
+    public boolean create(Product obj) {
+        String sql = "INSERT INTO product (name, quota, price) VALUES (?, ?, ?)";
+        return jdbcTemplate.update(sql, obj.getName(), obj.getQuota(), obj.getPrice()) > 0;
+    }
+
+    @Override
+    public Optional<List<Product>> fetchAll() {
+        String sql = "SELECT * FROM product";
+        Optional<List<Product>> optional;
+        try {
+            optional = Optional.of(jdbcTemplate
+                    .query(sql, new ProductMapper()));
+        } catch (Exception e) {
+            optional = Optional.empty();
+        }
+        return optional;
+    }
+
+    @Override
+    public Optional<Product> findById(Long id) {
+        String sql = "SELECT * FROM product WHERE id = ? LIMIT 1";
+        Optional<Product> optional;
+        try {
+            optional = Optional.ofNullable(jdbcTemplate
+                    .queryForObject(sql, new ProductMapper(), id));
+        } catch (Exception e) {
+            optional = Optional.empty();
+        }
+        return optional;
+    }
+
+    @Override
+    public boolean update(Product obj) {
+        Optional<Product> optional = findById(obj.getId());
+        if (optional.isEmpty()) return false;
+        else {
+            String sql = "UPDATE product SET name = ?, quota = ?, price = ? WHERE id = ?";
+            return jdbcTemplate.update(sql, obj.getName(), obj.getQuota(), obj.getPrice(), obj.getId()) > 0;
+        }
+    }
+
+    @Override
+    public boolean delete(Product obj) {
+        Optional<Product> optional = findById(obj.getId());
+        if (optional.isEmpty()) return false;
+        else {
+            String sql = "DELETE FROM product WHERE id = ?";
+            return jdbcTemplate.update(sql, obj.getId()) > 0;
+        }
+    }
+}
