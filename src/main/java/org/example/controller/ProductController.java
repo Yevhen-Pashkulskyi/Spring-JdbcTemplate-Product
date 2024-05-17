ab0a2489c0ec17573f251577d66a11ab497b4cdf
Yevhen-Pashkulskyi <pashkulskyevhen@gmail.com>
2024-05-17 11:32:10 +0300

    add product controller

--- src/main/java/org/example/controller/ProductController.java
+++ src/main/java/org/example/controller/ProductController.java
@@ -1,11 +1,15 @@
 package org.example.controller;
 
-import org.example.view.AppView;
-import org.example.view.ProductCreateView;
-import org.example.view.ProductReadByIdView;
-import org.example.view.ProductReadView;
+import org.example.entity.Product;
+import org.example.srvice.ProductService;
+import org.example.utils.AppStarter;
+import org.example.utils.Constants;
+import org.example.view.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
 
 @Component
 public class ProductController {
@@ -16,6 +20,51 @@ public class ProductController {
     @Autowired
     ProductReadView readView;
     @Autowired
-    ProductReadByIdView productReadByIdView;
+    ProductReadByIdView readByIdView;
+    @Autowired
+    ProductUpdateView updateView;
+    @Autowired
+    ProductDeleteView deleteView;
+    @Autowired
+    ProductService service;
 
+    public void getOption() {
+        int option = Integer.parseInt(menuView.getOption());
+        switch (option) {
+            case 1 -> create();
+            case 2 -> getAll();
+            case 3 -> getById();
+            case 4 -> update();
+            case 5 -> delete();
+            case 0 -> menuView.getOutput(Constants.APP_CLOSE_MSG);
+        }
+    }
+
+    private void create() {
+        String[] data = createView.getData();
+        Product product = new Product(data[0], data[1], data[2]);
+        createView.getOutput(service.create(product));
+    }
+
+    private void getAll() {
+        readView.getOutput(service.getAll());
+        AppStarter.startApp();
+    }
+
+    private void getById() {
+        readByIdView.getOutput(service.getById(readByIdView.getData()));
+    }
+
+    private void update() {
+        Map<String, String> data = updateView.getData();
+        Product product = new Product(Long.parseLong(data.get("id")),
+                data.get("name"), data.get("quota"), data.get("price"));
+        updateView.getOutput(service.update(product));
+        AppStarter.startApp();
+    }
+
+    private void delete() {
+        deleteView.getOutput(service.delete(deleteView.getData()));
+        AppStarter.startApp();
+    }
 }
